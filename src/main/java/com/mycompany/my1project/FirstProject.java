f31eecb868a8ee1d923185fa0c323d607c6e0a2d
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2019-04-14 14:25:31 +0200

    Initial commit.

---
+++ src/main/java/com/mycompany/my1project/FirstProject.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.my1project;
+
+ public class FirstProject {
+
+    public static void main(String[] args){
+        
+        int first_number;
+        
+        first_number = 5;
+        
+        // this is a integer
+        
+         System.out.println(first_number + " Is my favourite number");
+        
+        // This will print Hello World
+    }
+}
