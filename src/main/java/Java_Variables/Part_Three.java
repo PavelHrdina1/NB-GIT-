b230386744974a10aee2b75d74a8a4a92bec1d23
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2019-04-18 11:33:22 +0200

    Playing with git

---
+++ src/main/java/Java_Variables/Part_Three.java
@@ -0,0 +1,15 @@
+package Java_Variables;
+
+ public class Part_Three {
+ 
+   public static void main(String[] args){
+       
+       // Short and Float Variables
+       
+       // Short variable is used to store smaller numbers
+       // Short Variable can store numbers between -32,768/-32,768
+       
+       //Float Variables
+   }
+   
+}
