01a025de8065b68d19e7006bf8907fdf3ca4cf6a
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2019-04-26 20:48:08 +0200

    Playing with git

--- src/main/java/my1project/Hellow_World.java
+++ src/main/java/my1project/Hellow_World.java
@@ -1,5 +1,19 @@
 package my1project;
 
   public class Hellow_World {
-    
+      
+      public static void main(String[] args) {
+      
+             // This is aa one line comment 
+      
+            /* 
+                Multiple line coment
+            */
+      
+             /** Documentacion comments
+            *
+            */
+          
+      } 
+      
 }
