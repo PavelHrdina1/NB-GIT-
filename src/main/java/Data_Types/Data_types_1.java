38444e05e49b85410a359f198c64c5f5279cc1aa
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2019-05-02 21:06:42 +0200

    Playing with git

--- src/main/java/Data_Types/Data_types_1.java
+++ src/main/java/Data_Types/Data_types_1.java
@@ -1,7 +1,11 @@
 package Data_Types;
 
   public class Data_types_1 {
-    
-    
-    
+      
+      // Void method, does not give "back" somthing...
+      
+      // Then we have "primitive data types" or youst "Primit."
+      
+      // 
+      
 }
