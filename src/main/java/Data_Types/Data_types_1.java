c65c1132f0f5d0d2cb42897405e11b139be1127f
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2019-05-08 20:26:03 +0200

    Playing with git

--- src/main/java/Data_Types/Data_types_1.java
+++ src/main/java/Data_Types/Data_types_1.java
@@ -6,6 +6,46 @@ package Data_Types;
       
       // Then we have "primitive data types" or youst "Primit."
       
-      // 
+      // There are few data types. The moest used are cllases
+      
+      // Primitive data Types:
+      
+      /*
+      
+      Boolean: Defines type of loogick operator what can be only true or false
+               name of this type refers to Goerge Boola,
+               Boolean algebra
+      
+      */
+      
+      boolean XD = false; 
+      
+      /*
+      
+      Char: (Character) represents coding by Unicode
+      
+      */
+
+      /* 
+      
+      double is a data type for reprezantation of real 
+             numbers
+      
+      */
+      
+      /*
+      
+      int difines a type of whoule numbers 
+      
+      */
+      
+      /*
+      
+      long Big numbers from: -9 223 372 036 854 775 808,
+           to: +9 223 372 036 854 775 807 
+      
+      */
+      
+      // than we have, byte, short, float
       
 }
